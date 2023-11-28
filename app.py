6fc3dbfb3c6a70f497f3415efb70f90fb704152e
Daniel Giedraitis <91562130+DanielGiedraitis@users.noreply.github.com>
2023-11-28 01:10:46 +0000

    Login/Register working with database

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -9,6 +12,21 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Gied@localhost/DiversityDB'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a secret key
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 # Load Lemmas from Excel File
 def load_lemmas():
@@ -25,55 +43,110 @@ educational_lemmas, social_lemmas, technical_lemmas = load_lemmas()
 stop_words = set(stopwords.words('english'))
 porter = PorterStemmer()
 
-# Function to preprocess and tokenize text
+
 def preprocess_and_tokenize(text):
     words = word_tokenize(text.lower())
-
-    # Remove stop words and non-alphabetic words
     words = [porter.stem(word) for word in words if word.isalpha() and word not in stop_words]
     return words
+
 
 def analyze_text(text):
     words = preprocess_and_tokenize(text)
 
-    # Convert lemma sets to lowercase for case-insensitive comparison
     educational_lemmas_lower = {lemma.lower() for lemma in educational_lemmas}
     social_lemmas_lower = {lemma.lower() for lemma in social_lemmas}
     technical_lemmas_lower = {lemma.lower() for lemma in technical_lemmas}
 
-    # Count occurrences of lemmas from each category
     educational_count = sum(1 for word in words if word.lower() in educational_lemmas_lower)
     social_count = sum(1 for word in words if word.lower() in social_lemmas_lower)
     technological_count = sum(1 for word in words if word.lower() in technical_lemmas_lower)
 
     total_words = len(words)
 
-    # Calculate scores
     educational_score = educational_count / total_words
     social_score = social_count / total_words
     technological_score = technological_count / total_words
 
-    # Generate modified text with recommendations
     modified_text = f"Original Text:\n{text}\n\nRecommendations:\nPrint Recommendations."
 
     return educational_score, social_score, technological_score, modified_text
 
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        print(f"Received username: {username}, password: {password}")
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            print(f"Retrieved user: {user.username}, {user.password_hash}")
+
+            # Verify the password using check_password_hash
+            if check_password_hash(user.password_hash, password):
+                session['user_id'] = user.id
+                return redirect(url_for('index'))
+        
+        print("Invalid username or password.")
+        error = 'Invalid username or password.'
+        return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, email=email, password_hash=hashed_password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'user_id' in session:
+        return render_template('index.html')
+    else:
+        return redirect(url_for('login'))
+
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
     text_to_analyze = request.form['text']
-    
+
     educational_score, social_score, technological_score, modified_text = analyze_text(text_to_analyze)
 
     return render_template('result.html', text=text_to_analyze, edu_score=educational_score,
-                            social_score=social_score, tech_score=technological_score,
-                            modified_text=modified_text)
+                           social_score=social_score, tech_score=technological_score,
+                           modified_text=modified_text)
+
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, use_reloader=True)
+
+
 
 
 

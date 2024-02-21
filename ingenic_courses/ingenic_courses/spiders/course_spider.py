0d509775a29dc3ead4c1cd8f31dd8b562d332878
Daniel Giedraitis <daniel.giedraitis@gmail.com>
2024-02-21 14:53:20 +0000

    Extract Trinity college descriptions

--- ingenic_courses/ingenic_courses/spiders/course_spider.py
+++ ingenic_courses/ingenic_courses/spiders/course_spider.py
@@ -4,6 +4,10 @@ class CourseSpider(scrapy.Spider):
     name = 'courses'
     start_urls = ['https://ingenic.ie/?page_id=384']
 
+    # custom_settings = {
+    #     'DOWNLOAD_DELAY': 2  
+    # }
+
     def parse(self, response):
         # Extract course links
         course_links = response.css('tbody tr')
@@ -14,11 +18,32 @@ class CourseSpider(scrapy.Spider):
             for course_title_link in course_title_links:
                 course_title = course_title_link.css('::text').get().strip()
                 course_link = course_title_link.css('::attr(href)').get()
-                yield {
-                    'institution': institution.strip(),
-                    'level': level.strip(),
-                    'course_title': course_title,
-                    'course_link': course_link
-                }
+                # Call parse_course method to extract descriptions
+                yield scrapy.Request(course_link, callback=self.parse_course,
+                                     meta={'institution': institution.strip(),
+                                           'level': level.strip(),
+                                           'course_title': course_title})
+
+    def parse_course(self, response):
+        institution = response.meta['institution']
+        level = response.meta['level']
+        course_title = response.meta['course_title']
+        # Extract description based on institution
+        if institution.strip() == 'Trinity College Dublin':
+            description = response.css('.overview-full-section.flex-container p::text, .course-structure-full-section.flex-container p::text, .field--name-field-prospectus-about a::attr(href)').getall()
+            description = [desc.strip() for desc in description if desc.strip()] # Remove empty strings and strip whitespace
+            course_content = response.css('ADD CSS SELECOR ::text').getall()
+
+
+        description = ' '.join(description).strip()
+        yield {
+            'institution': institution,
+            'level': level,
+            'course_title': course_title,
+            'description': description,
+            'course_content': course_content
+        }
+
+
 
 

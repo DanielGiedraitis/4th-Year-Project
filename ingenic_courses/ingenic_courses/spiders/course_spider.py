9f6d54f25b6d2217ffbb8df6ab223831050edbb8
Daniel Giedraitis <daniel.giedraitis@gmail.com>
2024-02-22 21:05:27 +0000

    Extract Institute of Technology Sligo course descriptions

--- ingenic_courses/ingenic_courses/spiders/course_spider.py
+++ ingenic_courses/ingenic_courses/spiders/course_spider.py
@@ -86,6 +86,12 @@ class CourseSpider(scrapy.Spider):
             description = [desc.strip() for desc in description if desc.strip()]
             course_content = response.css('ADD CSS SELECOR ::text').getall()
 
+        # Institute of Technology Sligo
+        elif institution.strip() == 'Institute of Technology Sligo':
+            description = response.css('.course-tab-content[data-tab-title="Summary"] p::text, .course-tab-content[data-tab-title="Summary"] p a::attr(href)').getall()
+            description = [desc.strip() for desc in description if desc.strip()]
+            course_content = response.css('ADD CSS SELECOR ::text').getall()
+
         description = ' '.join(description).strip()
         yield {
             'institution': institution,

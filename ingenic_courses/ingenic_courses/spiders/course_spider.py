17959070e8a5ae670acdbcceb383a830a03f7bd3
Daniel Giedraitis <daniel.giedraitis@gmail.com>
2024-02-22 20:19:30 +0000

    Extract TUD - Tallaght Campus descriptions

--- ingenic_courses/ingenic_courses/spiders/course_spider.py
+++ ingenic_courses/ingenic_courses/spiders/course_spider.py
@@ -30,15 +30,20 @@ class CourseSpider(scrapy.Spider):
         course_title = response.meta['course_title']
         
         # Extract description based on institution
+        # Trinity College Dublin
         if institution.strip() == 'Trinity College Dublin':
             description = response.css('.overview-full-section.flex-container p::text, .course-structure-full-section.flex-container p::text, .field--name-field-prospectus-about a::attr(href)').getall()
             description = [desc.strip() for desc in description if desc.strip()] # Remove empty strings and strip whitespace
             course_content = response.css('ADD CSS SELECOR ::text').getall()
+
+        # Dublin City University  
         elif institution.strip() == 'Dublin City University':
             description = response.css('.overview-full-section.flex-container p::text').getall()
             description = [desc.strip() for desc in description if desc.strip()]
             course_content = response.css('.course-structure-full-section.flex-container ::text, .course-structure-full-section.flex-container a::attr(href)').getall()
             course_content = [course.strip() for course in course_content if course.strip()] 
+
+        # Dun Laoghaire Institute Of Art Design and Technology
         elif institution.strip() == 'Dun Laoghaire Institute Of Art Design and Technology':
             if course_title.strip() == 'BSc in Creative Computing':
                 description = response.css('div.intro::text, div.content-block.text-wrapper.wys-block h2:contains("What will I do?") + ul li::text').getall()
@@ -52,17 +57,28 @@ class CourseSpider(scrapy.Spider):
             module_details = response.css('.content-block.expander-block.accordion.faq.list-faq.panel-group .item-faq.panel .panel-body p::text').getall()
             course_content = [f"{headings[i]}: {module_details[i]}" for i in range(len(headings))]
             course_content = [course.strip() for course in course_content if course.strip()] 
+
+        # University College Dublin   
         elif institution.strip() == 'University College Dublin':
             description = response.css('div.row.queries div.col-xs-12.col-md-8.content p::text, div.row.queries div.col-xs-12.col-md-8.content p a::attr(href)').getall()
             description = [desc.strip() for desc in description if desc.strip()]
             course_content = response.css('ADD CSS SELECOR ::text').getall()
             course_content = [course.strip() for course in course_content if course.strip()] 
+
+        # Technological University Dublin - City Centre Campus 
         elif institution.strip() == 'Technological University Dublin - City Centre Campus':
             description = response.css('.content-accordion__item #content-accordion__item1 .well.copy p::text').getall() + \
                           response.css('.content-accordion__item #content-accordion__item2 .well.copy p::text').getall()
             description = [desc.strip() for desc in description if desc.strip()]
             course_content = response.css('ADD CSS SELECOR ::text').getall()
-            
+
+        # Technological University Dublin - Tallaght Campus
+        elif institution.strip() == 'Technological University Dublin - Tallaght Campus':
+            description = response.css('.content-accordion__item #content-accordion__item1 .well.copy p::text').getall() + \
+                          response.css('.content-accordion__item #content-accordion__item2 .well.copy p::text').getall()
+            description = [desc.strip() for desc in description if desc.strip()]
+            course_content = response.css('ADD CSS SELECOR ::text').getall()
+
         description = ' '.join(description).strip()
         yield {
             'institution': institution,
